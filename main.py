2d207799942f197a1cf54c73b3b0f49b26ba46e0
wsjeon <wonsjeon@kaist.ac.kr>
2017-04-18 02:48:36 +0900

    Modify attention module.

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ from data  import process_gridworld_data
 from model import Agent
 from utils import fmt_row
 
-import os; os.environ["CUDA_VISIBLE_DEVICES"]="0"
+import os; os.environ['CUDA_VISIBLE_DEVICES']='0'
 
-# Data
-tf.app.flags.DEFINE_string('input',           'data/gridworld_8.mat', 'Path to data')
-tf.app.flags.DEFINE_integer('imsize',         8,                      'Size of input image')
-# Parameters
-tf.app.flags.DEFINE_float('lr',               0.001,                  'Learning rate for RMSProp')
-tf.app.flags.DEFINE_integer('epochs',         30,                     'Maximum epochs to train for')
-tf.app.flags.DEFINE_integer('k',              10,                     'Number of value iterations')
-tf.app.flags.DEFINE_integer('ch_i',           2,                      'Channels in input layer')
-tf.app.flags.DEFINE_integer('ch_h',           150,                    'Channels in initial hidden layer')
-tf.app.flags.DEFINE_integer('ch_q',           10,                     'Channels in q layer (~actions)')
-tf.app.flags.DEFINE_integer('batchsize',      12,                     'Batch size')
-tf.app.flags.DEFINE_integer('statebatchsize', 10,                     'Number of state inputs for each sample (real number, technically is k+1)')
-# Misc.
-tf.app.flags.DEFINE_integer('seed',           0,                      'Random seed for numpy')
-tf.app.flags.DEFINE_integer('display_step',   1,                      'Print summary output every n epochs')
-tf.app.flags.DEFINE_string('logdir',          './tmp/vintf/',          'Directory to store tensorboard summary')
+flags = tf.app.flags
 
-agent = Agent()
+flags.DEFINE_string('DATA_PATH', './data/gridworld_8.mat', 'training data path')
+flags.DEFINE_integer('IM_SIZE', 8, 'size of maze image')
+flags.DEFINE_float('LR', 0.001, 'learning rate')
+flags.DEFINE_integer('MAX_EPOCHS', 30, 'maximum epoch for training network')
+flags.DEFINE_integer('NUM_VI', 10, 'Number of value iterations')
+flags.DEFINE_integer('batchsize',      12,                     'Batch size')
+flags.DEFINE_integer('NUM_STATES', 10,
+    'number of states for each sampled maze (technically, NUM_VI + 1)')
+flags.DEFINE_integer('SEED', 0, 'random seed for reproducibility')
+flags.DEFINE_string('LOGDIR', './tmp', 'path to save event files')
+
+def main():
+  """ Agent setting """
+  agent = Agent()
+
+  """ Learning """
+  agent.learn()
+
+  """ Test """
+  agent.test()
+  agent.sess.close()
+
+if __name__ == "__main__":
+  main()
--- model.py
+++ model.py
@@ -9,69 +9,11 @@ from utils import fmt_row
 import time
 
 class Agent(object):
-  def build_model(self):
-    FLAGS = self.FLAGS
-    FLAGS = tf.app.flags.FLAGS
-    self.X  = tf.placeholder(tf.float32, [None, FLAGS.imsize, FLAGS.imsize, FLAGS.ch_i])
-    self.S1 = tf.placeholder(tf.int32, [None, FLAGS.statebatchsize])
-    self.S2 = tf.placeholder(tf.int32, [None, FLAGS.statebatchsize])
-
-    k    = FLAGS.k    # Number of value iterations performed
-    ch_i = FLAGS.ch_i # Channels in input layer
-    ch_h = FLAGS.ch_h # Channels in initial hidden layer
-    ch_q = FLAGS.ch_q # Channels in q layer (~actions)
-    state_batch_size = FLAGS.statebatchsize # k+1 state inputs for each channel
-
-    tf.set_random_seed(0)
-
-    init = tf.random_normal_initializer(stddev = 0.01)
-
-    with slim.arg_scope([conv2d, fc], activation_fn = None, weights_initializer = init,
-        biases_initializer = None):
-      h = conv2d(self.X, ch_h, [3, 3], biases_initializer = init, scope = "conv1")
-      r = conv2d(h, 1, [3, 3], scope = "conv2")
-      v = tf.zeros(tf.shape(r))
-      for i in range(k + 1):
-        q = conv2d(tf.concat([r, v], axis = 3), ch_q, [3, 3], reuse = True, scope = "conv3")
-        v = tf.reduce_max(q, axis = 3, keep_dims = True)
-
-      q = tf.transpose(q, perm=[0, 3, 1, 2])
-      bs = tf.shape(q)[0]
-      rprn = tf.reshape(tf.tile(tf.reshape(tf.range(bs), [-1, 1]), [1, state_batch_size]), [-1])
-      ins1 = tf.cast(tf.reshape(self.S1, [-1]), tf.int32)
-      ins2 = tf.cast(tf.reshape(self.S2, [-1]), tf.int32)
-      idx_in = tf.transpose(tf.stack([ins1, ins2, rprn]), [1, 0])
-      q_out = tf.gather_nd(tf.transpose(q, [2, 3, 0, 1]), idx_in)
-
-      # add logits
-      self.logits = fc(q_out, 8, scope = "fc0")
-      # softmax output weights
-      self.net = tf.nn.softmax(self.logits)
-
-  def build_loss(self):
-    self.y = tf.placeholder(tf.int32, [None])
-    y_ = tf.cast(self.y, tf.int64)
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=self.logits, labels=y_)
-    self.loss = tf.reduce_mean(cross_entropy)
-
-  def build_optimizer(self):
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=self.FLAGS.lr, epsilon=1e-6, centered=True)
-    self.optimizer = optimizer.minimize(self.loss)
-
-  def build_summary(self):
-    self.cp = tf.cast(tf.argmax(self.net, axis = 1), tf.int32)
-    self.err = tf.reduce_mean(tf.cast(tf.not_equal(self.cp, self.y), dtype=tf.float32))
-    for var in tf.trainable_variables():
-      tf.summary.histogram(var.op.name, var)
-    self.summary_op = tf.summary.merge_all()
-    self.summary_writer = tf.summary.FileWriter(self.FLAGS.logdir)
-
   def __init__(self):
     self.FLAGS = tf.app.flags.FLAGS
-    FLAGS = self.FLAGS
-
-    np.random.seed(self.FLAGS.seed)
+    """ Fix random seed for reproducibility """
+#    np.random.seed(self.FLAGS.SEED)
+#    tf.set_random_seed(self.FLAGS.SEED)
 
     """ TensorFlow graph construction """
     self.build_model()
@@ -83,11 +25,63 @@ class Agent(object):
     self.sess = tf.Session()
     self.sess.run(tf.global_variables_initializer())
 
-    Xtrain, S1train, S2train, ytrain, Xtest, S1test, S2test, ytest = process_gridworld_data(input=FLAGS.input, imsize=FLAGS.imsize)
+  def build_model(self):
+    FLAGS = self.FLAGS
+    self.X = tf.placeholder(tf.float32, [None, FLAGS.IM_SIZE, FLAGS.IM_SIZE, 2])
+    self.S1 = tf.placeholder(tf.int32, [None, FLAGS.NUM_STATES])
+    self.S2 = tf.placeholder(tf.int32, [None, FLAGS.NUM_STATES])
+
+    init = tf.random_normal_initializer(stddev = 0.01)
+
+    with slim.arg_scope([conv2d, fc], activation_fn = None, weights_initializer = init,
+        biases_initializer = None):
+      h = conv2d(self.X, 150, [3, 3], biases_initializer = init, scope = "conv1")
+      r = conv2d(h, 1, [3, 3], scope = "conv2")
+      v = tf.zeros(tf.shape(r))
+      for i in range(FLAGS.NUM_VI + 1): # totally, NUM_VI value iterations and get q.
+        q = conv2d(tf.concat([r, v], axis = 3), 10, [3, 3], reuse = True, scope = "conv3")
+        v = tf.reduce_max(q, axis = 3, keep_dims = True)
+
+      X_idx = tf.reshape(tf.range(tf.shape(q)[0]), [-1, 1])
+      q_idx = tf.stack([
+        tf.reshape(tf.tile(X_idx, [1, FLAGS.NUM_STATES]), [-1]),
+        tf.reshape(self.S1, [-1]),
+        tf.reshape(self.S2, [-1])],
+        axis = 1)
+      q_out = tf.gather_nd(q, q_idx)
+
+      # add logits
+      self.logits = fc(q_out, 8, scope = "fc0")
+      # softmax output weights
+      self.net = tf.nn.softmax(self.logits)
+
+  def build_loss(self):
+    self.y = tf.placeholder(tf.int32, [None])
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits = self.logits, labels = self.y)
+    self.loss = tf.reduce_mean(cross_entropy)
+
+  def build_optimizer(self):
+    optimizer = tf.train.RMSPropOptimizer(learning_rate = self.FLAGS.LR,
+        epsilon = 1e-6, centered = True)
+    self.optimizer = optimizer.minimize(self.loss)
+
+  def build_summary(self):
+    self.cp = tf.cast(tf.argmax(self.net, axis = 1), tf.int32)
+    self.err = tf.reduce_mean(tf.cast(tf.not_equal(self.cp, self.y), dtype=tf.float32))
+    for var in tf.trainable_variables():
+      tf.summary.histogram(var.op.name, var)
+    self.summary_op = tf.summary.merge_all()
+    self.summary_writer = tf.summary.FileWriter(self.FLAGS.LOGDIR)
+
+  def learn(self):
+    FLAGS = self.FLAGS
+    Xtrain, S1train, S2train, ytrain, _, _, _, _ =\
+        process_gridworld_data(input = FLAGS.DATA_PATH, imsize=FLAGS.IM_SIZE)
 
     batch_size = FLAGS.batchsize
     print(fmt_row(10, ["Epoch", "Train Cost", "Train Err", "Epoch Time"]))
-    for epoch in range(int(FLAGS.epochs)):
+    for epoch in range(int(FLAGS.MAX_EPOCHS)):
         tstart = time.time()
         avg_err, avg_cost = 0.0, 0.0
         num_batches = int(Xtrain.shape[0]/batch_size)
@@ -99,14 +93,13 @@ class Agent(object):
                 fd = {self.X: Xtrain[i:j],
                       self.S1: S1train[i:j],
                       self.S2: S2train[i:j],
-                      self.y: ytrain[i * FLAGS.statebatchsize:j * FLAGS.statebatchsize]}
+                      self.y: ytrain[i * FLAGS.NUM_STATES:j * FLAGS.NUM_STATES]}
                 _, e_, c_ = self.sess.run([self.optimizer, self.err, self.loss], feed_dict=fd)
                 avg_err += e_
                 avg_cost += c_
         # Display logs per epoch step
-        if epoch % FLAGS.display_step == 0:
-            elapsed = time.time() - tstart
-            print(fmt_row(10, [epoch, avg_cost/num_batches, avg_err/num_batches, elapsed]))
+        elapsed = time.time() - tstart
+        print(fmt_row(10, [epoch, avg_cost/num_batches, avg_err/num_batches, elapsed]))
         summary = tf.Summary()
         summary.ParseFromString(self.sess.run(self.summary_op))
         summary.value.add(tag='Average error', simple_value=float(avg_err/num_batches))
@@ -114,11 +107,14 @@ class Agent(object):
         self.summary_writer.add_summary(summary, epoch)
     print("Finished training!")
 
+  def test(self):
+    FLAGS = self.FLAGS
+    _, _, _, _, Xtest, S1test, S2test, ytest =\
+        process_gridworld_data(input = FLAGS.DATA_PATH, imsize=FLAGS.IM_SIZE)
     # Test model
     correct_prediction = tf.cast(tf.argmax(self.net, 1), tf.int32)
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(tf.not_equal(correct_prediction, self.y), dtype=tf.float32))
-    acc = accuracy.eval({self.X: Xtest, self.S1: S1test, self.S2: S2test, self.y: ytest})
+    acc = self.sess.run(accuracy,
+        feed_dict = {self.X: Xtest, self.S1: S1test, self.S2: S2test, self.y: ytest})
     print "Accuracy: {}%".format(100 * (1 - acc))
-
-    self.sess.close()
